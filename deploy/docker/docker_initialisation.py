aee074025c3684fc41b4f0cd72276c09088153c5
NathanSmela <nsmela@hotmail.com>
2026-05-27 07:05:26 -0700

    refactor: added to tool.ruff.lint

--- deploy/docker/docker_initialisation.py
+++ deploy/docker/docker_initialisation.py
@@ -12,8 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-"""A set of utilities to manage the docker instance of qatrack
-"""
+"""A set of utilities to manage the docker instance of qatrack"""
 
 from glob import glob
 import os
@@ -24,7 +23,7 @@ import numpy as np
 
 from docker_utilities import QATRACK_DIRECTORY, wait_for_postrgres
 
-FIXTURES_GLOB: str = os.path.join(QATRACK_DIRECTORY, "fixtures/defaults/*/*")
+FIXTURES_GLOB: str = os.path.join(QATRACK_DIRECTORY, 'fixtures/defaults/*/*')
 
 
 def initialisation():
